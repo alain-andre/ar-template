450a3b9672ff7fe468912628cce5c1d2d574eb4b
Alain ANDRE <wordsbybird@gmail.com>
2014-06-10 16:53:02 +0200

    Mise en place de la generation grace à la fonction template

--- angular_template/angular_template_generator.rb
+++ angular_template/angular_template_generator.rb
@@ -10,82 +10,49 @@ class AngularTemplateGenerator < Rails::Generators::NamedBase
   #  - new.html.haml
   #  - _form.html.haml
   def copy_angular_template_file
-    copy_file "assets/javascripts/controller.js.erb", "app/assets/javascripts/controllers/#{class_name}.js"
-    # directory "assets/templates", "app/assets/templates/#{class_name}/"
+    template "assets/javascripts/controller.js.erb", "app/assets/javascripts/controllers/#{class_name}.js"
     template "assets/templates/index.html.haml.erb", "app/assets/templates/#{class_name}/index.html.haml"
+    template "assets/templates/show.html.haml.erb", "app/assets/templates/#{class_name}/show.html.haml"
+    template "assets/templates/new.html.haml.erb", "app/assets/templates/#{class_name}/new.html.haml"
   end
 
   # modify text of the copyed files
   def update_template_file
-    update_index get_camelized_class
-    update_edit get_camelized_class
-    update_show get_camelized_class
-    update_new get_camelized_class
-    update_form get_camelized_class
     update_init get_camelized_class
     update_application get_camelized_class
   end
 
   private
-    # Modify index
-    def update_index camelized_class
-      # gsub_file "app/assets/templates/#{class_name}/index.html.haml", /class_name/ do |match|
-      #   match << "#{class_name}"
-      # end
-      # gsub_file "app/assets/templates/#{class_name}/index.html.haml", /camelized_class/ do |match|
-      #   match << "#{camelized_class}"
-      # end
-    end
-
-    # Modify edit
-    def update_edit camelized_class
-    end
-
-    # Modify show
-    def update_show camelized_class
-    end
-
-    # Modify new
-    def update_new camelized_class
-    end
-
-    # Modify _form
-    def update_form camelized_class
-    end
 
     # Modify assets/javascripts/init.js or create if exists
     def update_init camelized_class
-      # if FileTest.exists?("app/assets/javascripts/init.js") then
-#       // module pour les controleurs de projects
-# var projectsModuleControllers = angular.module('projectsModuleControllers', []);
-# var defaultModuleControllers = angular.module('defaultModuleControllers', []);
-
-# // Création de l'application
-# var maSuperApp = angular.module('maSuperApp', ['ngRoute',
-#   'projectsModuleControllers', 'defaultModuleControllers'
-# ]);
-      # else
-#       put "Error, the application isn't templated for angularjs"
-      # end
+      if FileTest.exists?("app/assets/javascripts/init.js") then
+        inject_into_file 'app/assets/javascripts/init.js', :after => "'use strict';" do
+          "\n\t// module pour les controleurs de #{class_name}s\n\tvar #{get_camelized_class}sModuleControllers = angular.module('#{get_camelized_class}sModuleControllers', []);"
+        end
+        gsub_file 'app/assets/javascripts/init.js', /'ngRoute',/, "'ngRoute', '#{get_camelized_class}sModuleControllers',"
+      else
+        put "Error, the application isn't templated for angularjs"
+      end
     end
 
     # Modify assets/javascripts/application.js
     def update_application(camelized_class)
-  #     _file = "app/assets/javascripts/application.js"
-  #     if File.open(_file).each_line.any?{|line| line.include?('$routeProvider')}
-  #       inject_into_file _file, :after => "$routeProvider." do
-  # "\nwhen('/#{class_name}', {
-  #   templateUrl: 'assets/templates/#{class_name}/index.html',
-  #   controller: '#{camelized_class}Controller'
-  # }).
-  # when('/#{class_name}/:id', {
-  #   templateUrl: 'assets/templates/#{class_name}/show.html',
-  #   controller: '#{camelized_class}ShowController'
-  # })."
-  #       end
-  #     else
-  #       put "Error, the application isn't templated for angularjs"
-  #     end
+      _file = "app/assets/javascripts/application.js"
+      if File.open(_file).each_line.any?{|line| line.include?('$routeProvider')}
+        inject_into_file _file, :after => "$routeProvider." do
+        "\nwhen('/#{class_name}', {
+          templateUrl: 'assets/templates/#{class_name}/index.html',
+          controller: '#{camelized_class}sIndexController'
+        }).
+        when('/#{class_name}/:id', {
+          templateUrl: 'assets/templates/#{class_name}/show.html',
+          controller: '#{camelized_class}sShowController'
+        })."
+        end
+      else
+        put "Error, the application isn't templated for angularjs"
+      end
     end
 
     def get_camelized_class
