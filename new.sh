f9daa83750f65baefd78d2cddbf9ff8ce03f05f8
Alain ANDRE <wordsbybird@gmail.com>
2014-08-05 03:17:30 +0200

    Modification du shell (sortie de l'app générée)

--- new.sh
+++ new.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd ..
 rm -Rf test_app
-rails _4.0.0_ new test_app -m template.rb --skip-bundle
+rails _4.0.0_ new test_app -m ar-template/template.rb --skip-bundle
 cd test_app
-rails g angular_template book
+rails g angular_template test_book
