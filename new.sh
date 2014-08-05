8b77392caca02be536d2ea7501adf129e34349b1
Alain ANDRE <wordsbybird@gmail.com>
2014-08-05 04:37:59 +0200

    Ajout d'un model

--- new.sh
+++ new.sh
@@ -4,3 +4,5 @@ rm -Rf test_app
 rails _4.0.0_ new test_app -m ar-template/template.rb --skip-bundle
 cd test_app
 rails g angular_template test_book
+rails g model test_book nom:string:uniq appellation:string couleur:string 'price:decimal{10,2}'
+bundle exec rake db:migrate
