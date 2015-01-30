0ff9e02befefb967b1ad6ee197e931b1bd8360ad
Alain ANDRE <wordsbybird@gmail.com>
2015-01-30 05:47:51 +0100

    Amélioration de la partie CRUD

--- angular_scaffold/templates/config/locales/en.rb
+++ angular_scaffold/templates/config/locales/en.rb
@@ -1,19 +1,20 @@
 en:
   <%= class_name.underscore %>:
+    alphabetical: Alphabetical
+    create_button: Create
     creation_page: Creation page of <%= class_name.underscore %>
+    delete_button: Delete
+    detail_page: Detail page of <%= class_name.underscore %>
     edit_button: Edit
+    edit_button: Edit
+    edition_page: Edition page of <%= class_name.underscore %>
+    latest: Latest
+    list_of: List of <%= class_name.pluralize %>
     must_be_caracters_at_least: Must be at least 3 caracters
     must_be_caracters_maximum: Must be at maximum 20 caracters
-    textarea: Textarea
-    string: String
-    integer: Integer
-    edition_page: Edition page of <%= class_name.underscore %>
-    delete: Delete
-    list_of: List of <%= class_name.pluralize %>
-    detail_page: Detail page of <%= class_name.underscore %>
     new: New <%= class_name.underscore %>
     search: Search
     sort_by: Sort by
-    alphabetical: Alphabetical
-    latest: Latest
-    create: Create
+    string: String
+    update_button: Update
+    view_button: View
