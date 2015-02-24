c82567847d45a741382fa226cf6d0420e7d6310a
alain ANDRE <wordsbybird@gmail.com>
2015-02-24 21:21:11 +0100

    Fix some labels

--- new.sh
+++ new.sh
@@ -4,4 +4,4 @@ rm -Rf test_app
 rails _4.0.0_ new test_app -m ar-template/template.rb --skip-bundle --force
 cd test_app
 rails g auth_browserid 
-rails generate angular_scaffold compte name:string description:text price:decimal rdv:date valid:boolean hour:time create:datetime
+rails generate angular_scaffold compte nom:string description:text prix:decimal rdv:date validation:boolean heure:time creation:datetime
