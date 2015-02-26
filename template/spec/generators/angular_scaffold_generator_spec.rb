3ec55a235cc1253df1077fdffbc50452212b54ac
Alain ANDRE <wordsbybird@gmail.com>
2015-02-26 05:12:19 +0100

    Better spec for generators

--- template/spec/generators/angular_scaffold_generator_spec.rb
+++ template/spec/generators/angular_scaffold_generator_spec.rb
@@ -1,3 +1,4 @@
+require 'rake'
 require 'rails_helper'
 require 'ammeter/init'
 require 'rails/all'
@@ -7,21 +8,48 @@ require 'generators/angular_scaffold/angular_scaffold_generator'
 
 describe AngularScaffoldGenerator, :type => :generator do
   # Tell the generator where to put its output (what it thinks of as Rails.root)
-  dir = File.expand_path("../../../../../tmp", __FILE__)
+  dir = File.expand_path('../../../../../tmp', __FILE__)
   destination dir
   before do
     prepare_destination
-    # Implicitly loading app/controllers/api/v1/base.rb
-    FileUtils.mkdir_p("#{dir}/app/controllers/api/v1/")
-    File.open( "#{dir}/app/controllers/api/v1/base.rb", 'w' )
+  end
+
+  # Verifies the file's contents the given structure in HTML files
+  def test_html_structure
+    it { is_expected.to contain(/compte.nom/) }
+    it { is_expected.to contain(/compte.description/) }
+    it { is_expected.to contain(/compte.prix/) }
+    it { is_expected.to contain(/compte.rdv/) }
+    it { is_expected.to contain(/compte.validation/) }
+    it { is_expected.to contain(/compte.heure/) }
+    it { is_expected.to contain(/compte.creation/) }
+  end
+
+  # Verifies the file's contents the given structure in ruby files
+  def test_rb_structure
+    it { is_expected.to contain(/:nom/) }
+    it { is_expected.to contain(/:description/) }
+    it { is_expected.to contain(/:prix/) }
+    it { is_expected.to contain(/:rdv/) }
+    it { is_expected.to contain(/:validation/) }
+    it { is_expected.to contain(/:heure/) }
+    it { is_expected.to contain(/:creation/) }
   end
 
   # custom matchers make it easy to verify what the generator creates
   describe 'The generated files' do
-    before do
+
+    before do 
+      # Creates files modified in generator but created by template
+      FileUtils.mkdir_p("#{dir}/app/controllers/api/v1/")
+      File.open("#{dir}/app/controllers/api/v1/base.rb", 'w'){|file| file.write("Grape::API")}
+      FileUtils.mkdir_p("#{dir}/app/assets/javascripts/")
+      File.open("#{dir}/app/assets/javascripts/routes.js.coffee", 'w'){|file| file.write("stateProvider")}
+      # run generator
       run_generator %w(compte nom:string description:text prix:decimal rdv:date validation:boolean heure:time creation:datetime)
     end
-    describe 'controllers javascripts' do
+
+    describe 'in app/assets/javascripts/controllers/comptes_ctrl.js' do
       # file - gives you the absolute path where the generator will create the file
       subject { file('app/assets/javascripts/controllers/comptes_ctrl.js') }
       # is_expected_to exist - verifies the file exists
@@ -29,15 +57,98 @@ describe AngularScaffoldGenerator, :type => :generator do
       # is_expected_to contain - verifies the file's contents
       it { is_expected.to contain(/CompteListCtrl/) }
       it { is_expected.to contain(/CompteViewCtrl/) }
+      it { is_expected.to contain(/CompteCreateCtrl/) }
+      it { is_expected.to contain(/addCompte/) }
+      it { is_expected.to contain(/updateCompte/) }
+      it { is_expected.to contain(/deleteCompte/) }
     end
 
-    # describe 'the migration' do
-    #   subject { migration_file('db/migrate/create_posts.rb') }
+    describe 'in app/assets/javascripts/services/comptes_srv.js.coffee' do
+      subject { file('app/assets/javascripts/services/comptes_srv.js.coffee') }
+      it { is_expected.to exist }
+    end
 
-    #   # is_expected_to be_a_migration - verifies the file exists with a migration timestamp as part of the filename
-    #   it { is_expected_to exist }
-    #   it { is_expected_to contain /create_table/ }
-    # end
+    describe 'in app/assets/templates/comptes/index.html.haml' do
+      subject { file('app/assets/templates/comptes/index.html.haml') }
+      it { is_expected.to exist }
+      test_html_structure
+    end
+    describe 'in app/assets/templates/comptes/_filter.html.haml' do
+      subject { file('app/assets/templates/comptes/_filter.html.haml') }
+      it { is_expected.to exist }
+    end
+    describe 'in app/assets/templates/comptes/admin.html.haml' do
+      subject { file('app/assets/templates/comptes/admin.html.haml') }
+      it { is_expected.to exist }
+      test_html_structure
+    end
+    describe 'in app/assets/templates/comptes/edit.html.haml' do
+      subject { file('app/assets/templates/comptes/edit.html.haml') }
+      it { is_expected.to exist }
+    end
+    describe 'in app/assets/templates/comptes/show.html.haml' do
+      subject { file('app/assets/templates/comptes/show.html.haml') }
+      it { is_expected.to exist }
+      test_html_structure
+    end
+    describe 'in app/assets/templates/comptes/new.html.haml' do
+      subject { file('app/assets/templates/comptes/new.html.haml') }
+      it { is_expected.to exist }
+    end
+    describe 'in app/assets/templates/comptes/_form.html.haml' do
+      subject { file('app/assets/templates/comptes/_form.html.haml') }
+      it { is_expected.to exist }
+      test_html_structure
+    end
+
+    describe 'in app/controllers/api/v1/comptes.rb' do
+      subject { file('app/controllers/api/v1/comptes.rb') }
+      it { is_expected.to exist }
+      # Test API functions exists
+      it { is_expected.to contain(/Compte.all/) }
+      it { is_expected.to contain(/Compte.find\(params\[:id\]\)/) }
+      it { is_expected.to contain(/Compte.create!\(compte_params\)/) }
+      it { is_expected.to contain(/Compte.find\(params\[:id\]\).update\(compte_params\)/) }
+      it { is_expected.to contain(/Compte.find\(params\[:id\]\).destroy/) }
+      # Test structure is respected
+      test_rb_structure
+    end
+
+    describe 'in app/models/compte.rb' do 
+      subject { file('app/models/compte.rb') }
+      it { is_expected.to exist }
+    end
+
+    describe 'in config/locales/compte.en.yml' do 
+      subject { file('config/locales/compte.en.yml') }
+      it { is_expected.to exist }
+    end
+
+    describe 'the migration' do
+      subject { migration_file('db/migrate/create_comptes.rb') }
+      # is_expected_to be_a_migration - verifies the file exists with a migration timestamp as part of the filename
+      it { is_expected.to exist }
+      it { is_expected.to contain /create_table/ }
+      test_rb_structure
+    end
+
+    describe 'in app/assets/javascripts/routes.js.coffee' do
+      subject { file('app/assets/javascripts/routes.js.coffee') }
+      it { is_expected.to exist }
+      it { is_expected.to contain(/stateProvider/) }
+      it { is_expected.to contain(/\/comptes/) }
+      it { is_expected.to contain(/\/comptes\/:id\/view/) }
+      it { is_expected.to contain(/\/admin\/comptes/) }
+      it { is_expected.to contain(/\/admin\/comptes\/new/) }
+      it { is_expected.to contain(/\/admin\/comptes\/:id\/edit/) }
+    end
+
+    describe 'in app/controllers/api/v1/base.rb' do
+      subject { file('app/controllers/api/v1/base.rb') }
+      it { is_expected.to exist }
+      it { is_expected.to contain(/mount API::V1::Comptes/) }
+    end
+
   end
 
 end
--- template/spec/generators/auth_browserid_generator_spec.rb
+++ template/spec/generators/auth_browserid_generator_spec.rb
@@ -8,6 +8,8 @@ require 'generators/auth_browserid/auth_browserid_generator'
 describe AuthBrowseridGenerator, :type => :generator do
   # Tell the generator where to put its output (what it thinks of as Rails.root)
   destination File.expand_path("../../../../../tmp", __FILE__)
+  dir = File.expand_path('../../../../../tmp', __FILE__)
+  destination dir
   before do
     prepare_destination
   end
@@ -15,25 +17,59 @@ describe AuthBrowseridGenerator, :type => :generator do
   # custom matchers make it easy to verify what the generator creates
   describe 'The generated files' do
     before do
+      # Creates files modified in generator but created by template
+      File.open("#{dir}/Gemfile", 'w'){|file| file.write("gem 'devise'")}
+      FileUtils.mkdir_p("#{dir}/config/initializers/")
+      File.open("#{dir}/config/initializers/devise.rb", "w"){|file| file.write('end')}
+      FileUtils.mkdir_p("#{dir}/app/views/layouts/")
+      File.open("#{dir}/app/views/layouts/application.html.haml", "w")
+      FileUtils.mkdir_p("#{dir}/app/assets/javascripts/controllers/")
+      File.open("#{dir}/app/assets/javascripts/controllers/browserid_ctrl.js.coffee", "w")
+      # Run generator
       run_generator
     end
-    # describe 'controllers javascripts' do
-    #   # file - gives you the absolute path where the generator will create the file
-    #   subject { file('app/assets/javascripts/controllers/comptes_ctrl.js') }
-    #   # is_expected_to exist - verifies the file exists
-    #   it { is_expected_to exist }
-    #   # is_expected_to contain - verifies the file's contents
-    #   it { is_expected_to contain(/CompteListCtrl/) }
-    #   it { is_expected_to contain(/CompteViewCtrl/) }
-    # end
 
-    # describe 'the migration' do
-    #   subject { migration_file('db/migrate/create_posts.rb') }
+    describe 'in Gemfile' do
+      subject { file('Gemfile') }
+      it {is_expected.to contain(/devise_browserid_authenticatable/)}
+    end
+    
+    describe 'in config/initializers/browser_id.rb' do 
+      subject { file('config/initializers/browser_id.rb') }
+      it { is_expected.to exist }
+    end
 
-    #   # is_expected_to be_a_migration - verifies the file exists with a migration timestamp as part of the filename
-    #   it { is_expected_to exist }
-    #   it { is_expected_to contain /create_table/ }
-    # end
+    describe 'in config/initializers/devise.rb' do 
+      subject { file('config/initializers/devise.rb') }
+      it { is_expected.to exist }
+      it {is_expected.to contain(/config\.warden/)}
+    end
+
+    describe 'in app/views/layouts/application.html.haml' do 
+      subject { file('app/views/layouts/application.html.haml') }
+      it { is_expected.to exist }
+      it {is_expected.to contain(/browserid_js_tag/)}
+    end
+
+    describe 'in app/assets/javascripts/controllers/browserid_ctrl.js.coffee' do 
+      subject { file('app/assets/javascripts/controllers/browserid_ctrl.js.coffee') }
+      it { is_expected.to exist }
+      it {is_expected.to contain(/\/users\/sign_out/)}
+      it {is_expected.to contain(/\/users\/sign_in/)}
+    end
+
+    describe 'in app/views/layouts/_auth.html.haml' do 
+      subject { file('app/views/layouts/_auth.html.haml') }
+      it { is_expected.to exist }
+      it {is_expected.to contain(/\=render 'layouts\/auth_browserid'/)}
+    end
+
+    describe 'in app/views/layouts/_auth_browserid.html.haml' do 
+      subject { file('app/views/layouts/_auth_browserid.html.haml') }
+      it { is_expected.to exist }
+      it {is_expected.to contain(/user_signed_in?/)}
+    end
+
   end
 
 end
