fa5ef22a0632506d676e886af005d9651e7376c1
Alain ANDRE <wordsbybird@gmail.com>
2015-04-16 20:50:51 +0200

    Remove useless function

--- template/lib/generators/angular_scaffold/angular_scaffold_generator.rb
+++ template/lib/generators/angular_scaffold/angular_scaffold_generator.rb
@@ -2,15 +2,6 @@ class AngularScaffoldGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
 
   argument :model_attributes, type: :array, default: [], banner: "model:attributes"
-
-  # Initialize the generator accepting model attributes as arguments
-  def initialize(*args, &block)
-    super
-    @attributes = []
-    model_attributes.each do |attribute|
-      @attributes << Rails::Generators::GeneratedAttribute.new(*attribute.split(":")) if attribute.include?(":")
-    end
-  end
   
   # Creates the rails and angularjs controller
   def create_controller
